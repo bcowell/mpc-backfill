59feba9846cc324c77e81c942728f91b7671162f
Brayden Cowell <braydencowell@protonmail.com>
2022-03-06 19:43:03 -0500

    cleaned up with example

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # mpc-backfill
  
+# Setup
+1. Grab the latest MPC Fill exe from [here](https://github.com/MrTeferi/MPC-Fill/releases)
+2. With Ruby 3 installed 
+run `gem install nokogiri`
+
+## Running
+1. Configure the input decks and output name in the script.
+2. Call `ruby main.rb`
+3. Run the autofill.exe with the output XML in the same directory
+
+## Example
+![example_cart.jpg](example_cart.jpg)
--- main.rb
+++ main.rb
@@ -9,126 +9,225 @@ require 'nokogiri'
 # Ex.
 # front = [1, 2, ... 103]
 # back =  [3, 45, 78]
-def print_double_sided(deck_a_file_path, deck_b_file_path, output_file_path)
-  deck_a_file = File.open(deck_a_file_path)
-  deck_b_file = File.open(deck_b_file_path)
-  
-  doc_a = Nokogiri::XML(deck_a_file)
-  doc_b = Nokogiri::XML(deck_b_file)
+class BackFill
 
-  back_count_a = doc_a.search('//backs/card').count
-  back_count_b = doc_b.search('//backs/card').count
+  def initialize(
+    deck_a_file_path, 
+    deck_b_file_path, 
+    output_file_path
+  )
+    @output_file_path = output_file_path
+    @primary_doc = nil
+    @secondary_doc = nil
 
-  # first choose the deck that has less backs (more space to fill)
-  if (back_count_a < back_count_b)
-    primary_doc, secondary_doc = doc_a, doc_b
-  else
-    primary_doc, secondary_doc = doc_b, doc_a
+    open_files_and_prepare_xml_documents(deck_a_file_path, deck_b_file_path)
   end
 
-  # max amount of cards we can include
-  max_primary_bracket = primary_doc.search('//bracket').text.to_i
-  max_secondary_bracket = secondary_doc.search('//bracket').text.to_i
-
-  # ref to front and back root node
-  primary_front_node = primary_doc.at('//fronts')
-  primary_back_node = primary_doc.at('//backs')
-
-  # nodelists of all primary & secondary front/back cards
-  primary_front_nodelist = primary_doc.search('//fronts/card')
-  primary_back_nodelist = primary_doc.search('//backs/card')
-  secondary_front_nodelist = secondary_doc.search('//fronts/card')
-  secondary_back_nodelist = secondary_doc.search('//backs/card')
-
-  # array of slot ids of the already existing back cards
-  # Ex. [19, 55, 59, 91]
-  primary_back_slot_ids = array_of_back_slot_ids(primary_doc)
-  secondary_back_slot_ids = array_of_back_slot_ids(secondary_doc)
-
-  p "primary_back_slot_ids: #{primary_back_slot_ids}"
-  p "secondary_back_slot_ids: #{secondary_back_slot_ids}"
-
-  nodes_to_add_to_end = []
-
-  # 1. starting from last back fill the empty back slots using front of other deck
-  secondary_front_nodelist.each do |secondary_front_node|
-    front_slot_id = secondary_front_node.css('slots').text.to_i
-
-    if (primary_front_nodelist.count >= max_primary_bracket) ||
-      (primary_back_nodelist.count >= max_primary_bracket)
-      p 'Run out of room for cards in the primary deck'
-      p "front: #{primary_front_nodelist.count}/#{max_primary_bracket}"
-      p "back: #{primary_back_nodelist.count}/#{max_primary_bracket}"
-      break
-    end
-
-    if (primary_back_slot_ids.include?(front_slot_id))
-      # p 'the new slot_id already is taken by a primary card'
-      # unshift - add to front of leftovers so we can deal with these first
-      nodes_to_add_to_end.unshift(secondary_front_node)
-      # print_node(secondary_front_node)
-
-    elsif (secondary_back_slot_ids.include?(front_slot_id))
-      # p 'the new front to add has a back as well'
-      nodes_to_add_to_end.push(secondary_front_node)
-      # print_node(secondary_front_node)
-
-    else
-      primary_back_node.add_child(secondary_front_node)
-    end
+  def perform!
+    merge_primary_and_secondary_decks
+    save_combined_xml_output_to_file
   end
 
-  nodes_to_add_to_end.each do |secondary_front_node|
-    front_slot_id = secondary_front_node.css('slots').text.to_i
+  def open_files_and_prepare_xml_documents(deck_a_file_path, deck_b_file_path)
+    deck_a_file = File.open(deck_a_file_path)
+    deck_b_file = File.open(deck_b_file_path)
     
-    if (primary_front_nodelist.count >= max_primary_bracket) ||
-      (primary_back_nodelist.count >= max_primary_bracket)
-      p 'Run out of room for cards in the primary deck'
-      p "front: #{primary_front_nodelist.count}/#{max_primary_bracket}"
-      p "back: #{primary_back_nodelist.count}/#{max_primary_bracket}"
-      break
-    end
+    doc_a = Nokogiri::XML(deck_a_file)
+    doc_b = Nokogiri::XML(deck_b_file)
 
-    if (secondary_back_slot_ids.include?(front_slot_id))
-      p 'the new front to add has a back as well'
-      print_node(secondary_front_node)
-      # we need to add both front & back
-      # find first equal matching slot_id on front and back primary
-      # update slot_id of both and add to both
+    # primary deck should have fewer backs (more space to fill)
+    # TODO: technically this should be the max of the slot id in either
+    back_count_a = doc_a.search('//backs/card').count
+    back_count_b = doc_b.search('//backs/card').count
+    
+    if (back_count_a < back_count_b)
+      @primary_doc, @secondary_doc = doc_a, doc_b
     else
-      last_back_node = primary_doc.search('//backs/card').last()
-      last_back_node_slot_id = last_back_node.css('slots').text.to_i
-
-      secondary_front_node.at('slots').replace("<slots>#{last_back_node_slot_id + 1)}</slots>")
-      print_node(secondary_front_node)
-      break
-      # update slot id for front
-      # primary_back_node.add_child(secondary_front_node)
+      @primary_doc, @secondary_doc = doc_b, doc_a
     end
   end
 
-  File.write(output_file_path, primary_doc)
-end
 
-def array_of_back_slot_ids(doc)
-  back_slot_ids = []
+  def merge_primary_and_secondary_decks
+    # max amount of cards we can include
+    max_primary_bracket = @primary_doc.search('//bracket').text.to_i
+    max_secondary_bracket = @secondary_doc.search('//bracket').text.to_i
 
-  doc.search('//backs/card/slots').each do |el|
-    back_slot_ids << el.text.to_i
+    primary_back_node = @primary_doc.at('//backs')
+    secondary_front_nodelist = @secondary_doc.search('//fronts/card')
+
+    # array of slot ids of the already existing back cards
+    # Ex. [19, 55, 59, 91]
+    primary_back_slot_ids = array_of_back_slot_ids(@primary_doc)
+    secondary_back_slot_ids = array_of_back_slot_ids(@secondary_doc)
+
+    p "primary_back_slot_ids: #{primary_back_slot_ids}"
+    p "secondary_back_slot_ids: #{secondary_back_slot_ids}"
+
+    nodes_to_add_to_end = []
+
+    # fill the empty back slots of primary using front of secondary
+    # if there's a conflict of ids or we the card to add includes a back
+    # store and we'll worry about those after
+    secondary_front_nodelist.each do |secondary_front_node|
+      front_slot_ids = get_slot_ids(secondary_front_node)
+
+      # the back slot is already taken by a primary card
+      if (primary_back_slot_ids & front_slot_ids).size > 0
+        # unshift - add to front of leftovers so we can deal with these first
+        nodes_to_add_to_end.unshift(secondary_front_node)
+
+      # the new front to add has a back as well
+      elsif (secondary_back_slot_ids & front_slot_ids).size > 0
+        nodes_to_add_to_end.push(secondary_front_node)
+
+      else
+        primary_back_node.add_child(secondary_front_node)
+      end
+    end
+
+    nodes_to_add_to_end.each do |secondary_front_node|
+      front_slot_ids = get_slot_ids(secondary_front_node)
+
+      # the new front has a back tied to it
+      if (secondary_back_slot_ids & front_slot_ids).size > 0
+        add_both_secondary_front_and_back_to_primary(secondary_front_node, front_slot_ids.first)
+
+      # back slot id was already taken just add to end with new slot id
+      else
+        add_secondary_front_to_primary(secondary_front_node, front_slot_ids)
+      end
+    end
+
+    # TODO: fix slot ids with array
+    # otherwise this is not the right count
+    count_and_set_card_quantity
   end
 
-  back_slot_ids
-end
-
-def print_node(node)
-  hash = {}
-  node.elements.each do |el|
-    hash[el.name] = el.text
+  def save_combined_xml_output_to_file
+    File.write(@output_file_path, @primary_doc)
   end
-  p hash
+
+  def count_and_set_card_quantity
+    max_primary_front_slot_id = 0
+    max_primary_back_slot_id = 0
+
+    @primary_doc.search('//fronts/card').map do |front_card_node|
+      max_slot_id = get_slot_ids(front_card_node).max
+      if max_slot_id > max_primary_front_slot_id 
+        max_primary_front_slot_id = max_slot_id
+      end
+    end
+    @primary_doc.search('//backs/card').map do |back_card_node|
+      max_slot_id = get_slot_ids(back_card_node).max
+      if max_slot_id > max_primary_back_slot_id 
+        max_primary_back_slot_id = max_slot_id
+      end
+    end
+
+    new_quantity = [max_primary_front_slot_id, max_primary_back_slot_id].max
+    @primary_doc.at('//quantity').content = new_quantity
+  end
+
+  def add_secondary_front_to_primary(secondary_front_node, front_slot_ids)
+    primary_back_node = @primary_doc.at('//backs')
+
+    front_slot_id = nil
+
+    last_back_node = @primary_doc.search('//backs/card').last()
+    last_back_node_slot_ids = get_slot_ids(last_back_node)
+    new_slot_id = last_back_node_slot_ids.max + 1
+
+    # throw an error if the new slot id is outside of max_primary_bracket
+
+    if front_slot_ids.size == 1
+      front_slot_id = front_slot_ids.first
+      secondary_front_node.at('slots').content = new_slot_id
+    else
+      p 'card has multiple slot ids'
+      print_node(secondary_front_node)
+    end
+
+    primary_back_node.add_child(secondary_front_node)
+  end
+
+  def add_both_secondary_front_and_back_to_primary(secondary_front_node, front_slot_id)
+    primary_front_node = @primary_doc.at('//fronts')
+    primary_back_node = @primary_doc.at('//backs')
+
+    # we need to add both front & back
+    # find first equal matching slot_id on front and back primary
+    # update slot_id of both and add to both
+
+    last_front_node = @primary_doc.search('//fronts/card').last()
+    last_back_node = @primary_doc.search('//backs/card').last()
+
+    front_node_slot_id = get_slot_id(last_front_node)
+    back_node_slot_id = get_slot_id(last_back_node)
+
+    new_slot_id = [front_node_slot_id, back_node_slot_id].max + 1
+
+    # throw an error if the new slot id is outside of max_primary_bracket
+    
+    # update slot ids
+    secondary_back_node_slot_id = @secondary_doc.at("//backs/card/slots[text()=#{front_slot_id}]")
+    secondary_back_node_slot_id.content = new_slot_id
+    secondary_front_node.at('slots').content = new_slot_id
+    
+    secondary_back_node = secondary_back_node_slot_id.parent
+
+    primary_back_node.add_child(secondary_front_node)
+    primary_front_node.add_child(secondary_back_node)
+  end
+
+  def get_slot_id(node)
+    query = node.css('slots').text
+    raise "Slot id cannot be an array" if query.include? ','
+    query.to_i
+  end
+
+  def get_slot_ids(node)
+    query = node.css('slots').text
+    if query.include? ','
+      query.split(",").map(&:to_i)
+    else
+      [query.to_i]
+    end
+  end
+
+  def pluck_max_slot_id(slot)
+    id = slot.text
+    if id.include? ','
+      id.split(",").map(&:to_i)
+    else
+      [id.to_i]
+    end
+  end
+
+  def array_of_back_slot_ids(doc)
+    back_slot_ids = []
+
+    doc.search('//backs/card').each do |node|
+      new_ids = get_slot_ids(node)
+      back_slot_ids += new_ids
+    end
+
+    back_slot_ids
+  end
+
+  def print_node(node)
+    hash = {}
+    node.elements.each do |el|
+      hash[el.name] = el.text
+    end
+    p hash
+  end
 end
 
+# TODO: take this input from CLI
 deck_a_file_path = './example-decks/w_weenie.xml'
 deck_b_file_path = './example-decks/vial_krark.xml'
 output_file_path = './output/w_weenie_vial_krark.xml'
-print_double_sided(deck_a_file_path, deck_b_file_path, output_file_path)
+
+backfill = BackFill.new(deck_a_file_path, deck_b_file_path, output_file_path)
+backfill.perform!
